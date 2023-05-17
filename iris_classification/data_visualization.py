f9c1caa8935508c8a6d0f4ee1f6b7bafb9422b8a
takutak <63490418+takutak@users.noreply.github.com>
2023-05-17 23:05:50 +0900

    Update data_visualization.py

--- iris_classification/data_visualization.py
+++ iris_classification/data_visualization.py
@@ -7,26 +7,29 @@ x_setosa = x_org[y_org == 0]
 x_versicolor = x_org[y_org == 1]
 x_virginica = x_org[y_org == 2]
 
-plt.scatter(x_setosa[:, 0], x_setosa[:, 1], marker = 'x', c = 'red', label = 'setosa')
-plt.scatter(x_versicolor[:, 0], x_versicolor[:, 1], marker = 'o', c = 'blue', label = 'versicolor')
-plt.scatter(x_virginica[:, 0], x_virginica[:, 1], marker = ',',c = 'green', label = 'virginica' )
+fig = plt.figure(figsize=(12, 6))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
 
-plt.xlabel('sepal length [cm]')
-plt.ylabel('sepal width [cm]')
+ax1.scatter(x_setosa[:, 0], x_setosa[:, 1], marker = 'x', c = 'red', label = 'setosa')
+ax1.scatter(x_versicolor[:, 0], x_versicolor[:, 1], marker = 'o', c = 'blue', label = 'versicolor')
+ax1.scatter(x_virginica[:, 0], x_virginica[:, 1], marker = ',',c = 'green', label = 'virginica' )
 
-plt.legend(fontsize = 12)
-plt.title('iris sepal data')
+ax1.set_xlabel('sepal length [cm]')
+ax1.set_ylabel('sepal width [cm]')
 
-plt.show()
+ax1.legend(fontsize = 12)
+ax1.set_title('iris sepal data')
 
-plt.scatter(x_setosa[:, 2], x_setosa[:, 3], marker = 'x', c = 'red', label = 'setosa')
-plt.scatter(x_versicolor[:, 2], x_versicolor[:, 3], marker = 'o', c = 'blue', label = 'versicolor')
-plt.scatter(x_virginica[:, 2], x_virginica[:, 3], marker = ',',c = 'green', label = 'virginica' )
 
-plt.xlabel('petal length [cm]')
-plt.ylabel('petal width [cm]')
+ax2.scatter(x_setosa[:, 2], x_setosa[:, 3], marker = 'x', c = 'red', label = 'setosa')
+ax2.scatter(x_versicolor[:, 2], x_versicolor[:, 3], marker = 'o', c = 'blue', label = 'versicolor')
+ax2.scatter(x_virginica[:, 2], x_virginica[:, 3], marker = ',',c = 'green', label = 'virginica' )
 
-plt.legend(fontsize = 12)
-plt.title('iris petal data')
+ax2.set_xlabel('petal length [cm]')
+ax2.set_ylabel('petal width [cm]')
+
+ax2.legend(fontsize = 12)
+ax2.set_title('iris petal data')
 
 plt.show()
