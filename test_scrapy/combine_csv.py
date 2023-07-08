55fbc1073361deb68da07fa9e7d0a4fd326f193c
H. Kumagai <63490418+takutak@users.noreply.github.com>
2023-07-08 15:58:12 +0900

    Update combine_csv.py

--- test_scrapy/combine_csv.py
+++ test_scrapy/combine_csv.py
@@ -1,4 +1,5 @@
-import csv
+import pandas as pd
+
 print("###  ファイル名は拡張子まで含めて記述してください。  ###")
 # csvファイルの名前のリストを作成
 n = int(input("結合したいcsvファイルの個数を入力："))
@@ -7,30 +8,23 @@ for i in range(n):
     input_file = input("csvファイルの名前を入力：")
     csv_files.append(input_file)
 
-# すでに見た 'instruction' の値を保存するセット
+# すでに読み込んだ 'instruct' の値を保存するセット
 seen_instructions = set()
 
 output_file_name = input("出力するcsvファイルの名前を入力：")
-# 出力用のCSVファイルを開く
-with open(output_file_name, 'w', newline='', encoding='utf-8') as outfile:  # ここで 'utf-8' を指定
-    writer = csv.writer(outfile)
 
-    for file in csv_files:
-        with open(file, 'r', encoding='utf-8') as infile:  # ここでも 'utf-8' を指定
-            reader = csv.reader(infile)
+# 結合後のデータを保存するデータフレーム
+df_output = pd.DataFrame()
 
-            # header 行を処理
-            if file == csv_files[0]:
-                header = next(reader)
-                writer.writerow(header)
-            else:
-                # header 行をスキップ
-                next(reader)
+for file in csv_files:
+    df = pd.read_csv(file, encoding='utf-8')
 
-            for row in reader:
-                instruction = row[0]  # 'instruction' 列が最初であると仮定
+    if 'instruct' in df.columns:
+        df = df[~df['instruct'].isin(seen_instructions)]
+        seen_instructions.update(df['instruct'].tolist())
 
-                if instruction not in seen_instructions:
-                    writer.writerow(row)
-                    seen_instructions.add(instruction)
-        print(f"{file}は正常に追加しました。")
+    df_output = pd.concat([df_output, df])
+
+    print(f"{file}は正常に追加しました。")
+
+df_output.to_csv(output_file_name, index=False, encoding='utf-8')
